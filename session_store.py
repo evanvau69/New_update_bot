46a5f1c6ae8da346c45c5d8f7042bd0407266444
evanvau69 <bitulsarker3490@gmail.com>
2025-05-24 11:31:24 +0600

    Delete session_store.py

--- session_store.py
+++
@@ -1,8 +0,0 @@
-# session_store.py
-user_sessions = {}
-
-def save_session(user_id, sid, token):
-    user_sessions[user_id] = {"sid": sid, "token": token}
-
-def get_session(user_id):
-    return user_sessions.get(user_id)
